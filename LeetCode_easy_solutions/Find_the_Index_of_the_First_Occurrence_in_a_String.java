1aec849cbc1822cfa0f9c43456b2d8144072b229
Giridhar Patil <140000019+patilgiridhar333@users.noreply.github.com>
2023-10-27 11:23:31 +0530

    Create Find_the_Index_of_the_First_Occurrence_in_a_String.java

---
+++ LeetCode_easy_solutions/Find_the_Index_of_the_First_Occurrence_in_a_String.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(!haystack.contains(needle)){
+            return -1;
+        }
+        else{
+           return haystack.indexOf(needle);
+        }
+    }
+}
