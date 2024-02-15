744c2fdf935d3afa3f2af4a2a74093f9bc4b3130
Giridhar Patil <140000019+patilgiridhar333@users.noreply.github.com>
2024-02-15 21:27:30 +0530

    Update Best_Time_To_Buy_And_Sell_Stocks.java

--- LeetCode_easy_solutions/Best_Time_To_Buy_And_Sell_Stocks.java
+++ LeetCode_easy_solutions/Best_Time_To_Buy_And_Sell_Stocks.java
@@ -1,1 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=Integer.MAX_VALUE;
+        int profit=0;
+        int pt=0;
 
+        for(int i=0;i<prices.length;i++){
+            if(min>prices[i]){
+                min=prices[i];
+            }
+            pt=prices[i]-min;
+            if(pt>profit){
+                profit=pt;
+            }
+
+        }
+        return profit;
+    }
+}
